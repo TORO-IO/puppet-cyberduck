9076776a4ea40a888622e557310efaad91001ca2
Pongstr <twiz.tickler@gmail.com>
2015-07-08 07:33:09 +0800

    Updates to Cyberduck Puppet
    - updated version link
    - added `ensure => 'installed'`to fix
      `Can't read PKZip signature` error

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,8 @@
 #   include cyberduck
 class cyberduck {
   package { 'Cyberduck':
+    ensure     => 'installed',
     provider   => 'compressed_app',
-    source     => 'http://cyberduck.ch/Cyberduck-4.3.1.zip'
+    source     => 'https://a48823c7ec3cf4539564-60c534a1284a12ce74ef84032e9b4e46.ssl.cf1.rackcdn.com/Cyberduck-4.7.1.zip',
   }
 }
--- spec/classes/cyberduck_spec.rb
+++ spec/classes/cyberduck_spec.rb
@@ -3,8 +3,9 @@ require 'spec_helper'
 describe 'cyberduck' do
   it do
     should contain_package('Cyberduck').with({
-      :source   => 'http://cyberduck.ch/Cyberduck-4.3.1.zip',
-      :provider => 'compressed_app'
+      :ensure   => 'installed',
+      :source   => 'https://a48823c7ec3cf4539564-60c534a1284a12ce74ef84032e9b4e46.ssl.cf1.rackcdn.com/Cyberduck-4.7.1.zip',
+      :provider => 'compressed_app',
     })
   end
 end
